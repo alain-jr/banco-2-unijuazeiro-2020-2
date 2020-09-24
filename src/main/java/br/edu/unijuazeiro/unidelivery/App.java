be1adfc9936ac1634b45ea72c9e7b0eaa3d23f19
Iury Teixeira <franciury@gmail.com>
2020-09-24 19:50:24 -0300

    CRUD

--- src/main/java/br/edu/unijuazeiro/unidelivery/App.java
+++ src/main/java/br/edu/unijuazeiro/unidelivery/App.java
@@ -1,5 +1,7 @@
 package br.edu.unijuazeiro.unidelivery;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public final class App {
@@ -7,6 +9,35 @@ public final class App {
     }
 
     public static void main(String[] args) {
-        System.out.println("Primeira Conexao: " + Persistence.createEntityManagerFactory("unit-unijuazeiro"));
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("unit-unijuazeiro");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        // INSERT
+        // Customer c = new Customer(2, "João Alencar");
+        // em.persist(c);
+
+        // SELECT
+        // Customer c = em.find(Customer.class, 2);
+        // System.out.println("Codigo " + c.getCode());
+        // System.out.println("Nome " + c.getName());
+
+        // UPDATE AND REMOVE
+        Customer c = em.find(Customer.class, 1);
+        System.out.println("Nome " + c.getName());
+
+        //em.merge(c); // envia para banco
+        //em.refresh(c); // traz pra aplicacao/atualiza o objeto na aplicacao
+        em.remove(c);
+
+        em.getTransaction().commit();
+
+        em.close();
+
+        emf.close();
+
+        System.out.println("objeto na aplicação: "+c.getName());
+
     }
 }
--- src/main/java/br/edu/unijuazeiro/unidelivery/Customer.java
+++ src/main/java/br/edu/unijuazeiro/unidelivery/Customer.java
@@ -3,10 +3,17 @@ package br.edu.unijuazeiro.unidelivery;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // gets and sets
+@NoArgsConstructor // construtor sem argumentos
+@AllArgsConstructor // construtor com todos argumentos com base nos atributos da classe
+@Entity // indica que essa classe representa uma ENTIDADE
 public class Customer {
 
-    @Id
+    @Id // indica que CODE é o campo chave-primária
     private Integer code;
 
     private String name;
